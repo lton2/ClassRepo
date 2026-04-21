7b171910f84c0a8090ce0570b79a95b9e709c765
lilliope <loi.nguyen1099@gmail.com>
2026-04-21 02:14:40 -0400

    Week 5 exercises

---
+++ Ch10Ex1ToDoList/ToDoList/Migrations/20260421051527_UpdateToDoModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ch10ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateToDoModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusId",
+                table: "ToDos",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "StatusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatusId",
+                table: "ToDos",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_Statuses_StatusId",
+                table: "ToDos",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "StatusId");
+        }
+    }
+}
